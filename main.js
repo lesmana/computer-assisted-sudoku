8a9f21bdefc37c8dad4a09153404b432937402b2
Lesmana Zimmer <lesmana@gmx.de>
2016-05-06 09:33:55 +0200

    explicit event

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-window.onload = function() {
+bodyOnload = function() {
   var sudokuDiv = document.getElementById('sudoku')
   var sudokuGrid = createHtmlGrid()
   sudokuDiv.replaceChild(sudokuGrid, sudokuDiv.firstChild)
